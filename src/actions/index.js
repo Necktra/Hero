ce2275646cd8c9cc27dce78ccfa2bae49e36099c
Ira <hallo.ween@mail.ru>
2021-12-07 15:28:18 +0300

    add createAsyncThunk for heroesSlice

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,4 @@
 import { filtersFetched, filtersFetching, filtersFetchingError } from "../components/heroesFilters/filtersSlice";
-import { heroesFetched, heroesFetching, heroesFetchingError } from "../components/heroesList/heroesSlice";
-
-export const fetchHeroes = (request) => (dispatch) => {
-    dispatch(heroesFetching());
-    request("http://localhost:3001/heroes")
-        .then(data => dispatch(heroesFetched(data)))
-        .catch(() => dispatch(heroesFetchingError()))
-}
 
 export const fetchFilters = (request) => (dispatch) => {
     dispatch(filtersFetching());
