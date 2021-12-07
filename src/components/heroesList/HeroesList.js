8403fa89d1987b73a083ad95bd43807f21ade71b
Ira <hallo.ween@mail.ru>
2021-12-07 17:12:31 +0300

    add createEntityAdapter for heroesSlice

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,28 +1,13 @@
 import {useHttp} from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
-//import { fetchHeroes } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
-
 import './heroesList.scss';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { createSelector } from '@reduxjs/toolkit';
-import { fetchHeroes, heroDeleted } from './heroesSlice';
+import { fetchHeroes, filteredHeroesSelector, heroDeleted } from './heroesSlice';
 
 const HeroesList = () => {
-
-    const filteredHeroesSelector = createSelector(
-        (state) => state.filters.activeFilter,
-        (state) => state.heroes.heroes,
-        (filter, heroes) => {
-            if (filter === 'all') {
-                return heroes;
-            } else {
-            return heroes.filter((el)=>el.element === filter)}
-        }
-    );
 
     const filteredHeroes = useSelector(filteredHeroesSelector);
     const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
